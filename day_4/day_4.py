a4cac590682fcde482682afae03272f62d52f020
dgallegos47 <75699528+dgallegos47@users.noreply.github.com>
2021-12-15 10:23:59 -0500

    Add files via upload

--- day_4/day_4.py
+++ day_4/day_4.py
@@ -1,47 +1,83 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sat Dec  4 19:46:43 2021
+Created on Tue Dec 14 15:30:13 2021
 
-@author: domin
+@author: dgallegos
 """
 
-class Board:
+class Board():
     def __init__(self, board):
-        self.board = board
+        self.rows = []
+        self.cols = []
+        for i in range(5):
+            self.rows.append(board[i])
+            col = []
+            for j in range(5):
+                col.append(board[j][i])
+            self.cols.append(col)
         
-        row = []
-        for i in range(len(board)):
-            row.append(int(x) for x in board[i].split(' '))
+        self.numbers = [item for row in self.rows for item in row]
             
-        self.rows = row
+    def bingo_check(self, number_list):
+        row_bingo = False
+        col_bingo = False
+        for row in self.rows:
+            row_bingo = row_bingo or all(number in number_list for number in row)
+        for col in self.cols:
+            col_bingo = col_bingo or all(number in number_list for number in col)
+            
+        return row_bingo or col_bingo
+    
+    def board_score(self, number_list):
+        last_number = number_list[-1]
+        unmarked_numbers = [x for x in self.numbers if x not in number_list]
 
-def build_board(first, last, board_values):
-    board = []
-    for i in range(first, last):
-        board.append(board_values[i])
+        return sum(unmarked_numbers)*last_number
         
-        
-    return board
-
-with open('day_4_test.txt') as fh:
-    file_lines = fh.readlines()
-lines = [x.strip('\n') for x in file_lines]
+            
+with open('day_4.txt') as fh:
+    data = fh.readlines()
     
-draw_order = [int(x) for x in file_lines[0].split(',')]
+number_list = [int(x) for x in str(data[0].replace('\n', '')).split(',')]
 
-board_ranges = list(range(2, len(file_lines), 6))
+empty_row = list(range(1, len(data), 6))
 
-board_list = []
+boards = []
 
-for i in range(len(board_ranges)):
-    board_list.append(build_board(board_ranges[i], board_ranges[i] + 5, lines))
+for i in range(len(empty_row)):
+    sub_list = []
+    for j in range(1,6):
+        # sub_list.append(data[empty_row[i] + j].replace('\n',''))
+        line = data[empty_row[i] + j].replace('\n', '').split(' ')
+        trimmed_line = list(filter(''.__ne__, line))
+        sub_list.append([int(x) for x in trimmed_line])
+        # sub_list.append([int(x) for x in data[empty_row[i] + j].replace('\n', '').split(' ')])
     
-b1 = Board(board_list[1])
+    boards.append(Board(sub_list))
+    
+bingo = False
+i = 0
+call_list = []
+call_list.append(number_list[0])
+
+bingo_scores = []
+last_removed_score = 0
+
+while bingo == False:
+    for board in boards:
+        if board.bingo_check(call_list) == True:
+            bingo_scores.append(board.board_score(call_list))
+            boards.remove(board)
+            # bingo = True
+    
+    if i != len(number_list) - 1:
+        i += 1
+        call_list.append(number_list[i])
+    else:
+        bingo = True
 
     
-
+    
     
 
-    
 
-    
